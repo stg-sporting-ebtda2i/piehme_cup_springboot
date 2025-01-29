c4d3a4ace5fcf51b6cb7b6bc3a5905b8aa5b2db6
Armia Joseph <armia.joseph35@gmail.com>
2025-01-29 13:11:35 +0200

    init springboot

---
+++ src/main/java/com/stgsporting/piehmecup/PiehmecupApplication.java
@@ -0,0 +1,13 @@
+package com.stgsporting.piehmecup;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PiehmecupApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PiehmecupApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/stgsporting/piehmecup/PiehmecupApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stgsporting.piehmecup;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PiehmecupApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
