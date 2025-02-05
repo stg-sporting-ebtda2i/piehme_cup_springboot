fc8001bfb32fd2bc0c76794b58a2aabae0534361
Armia Joseph <armia.joseph35@gmail.com>
2025-02-05 23:20:28 +0200

    feat: added players exceptions

---
+++ src/main/java/com/stgsporting/piehmecup/exceptions/PlayerNotFoundException.java
@@ -0,0 +1,7 @@
+package com.stgsporting.piehmecup.exceptions;
+
+public class PlayerNotFoundException extends RuntimeException {
+    public PlayerNotFoundException(String message) {
+        super(message);
+    }
+}
---
+++ src/main/java/com/stgsporting/piehmecup/exceptions/UnknownPositionException.java
@@ -0,0 +1,7 @@
+package com.stgsporting.piehmecup.exceptions;
+
+public class UnknownPositionException extends RuntimeException {
+    public UnknownPositionException(String message) {
+        super(message);
+    }
+}
