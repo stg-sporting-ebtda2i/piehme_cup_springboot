4d229efd14b76ca8801cf778a6153e59d101d253
Armia Joseph <armia.joseph35@gmail.com>
2025-02-14 21:38:36 +0200

    feat: attendance

---
+++ src/main/java/com/stgsporting/piehmecup/exceptions/AttendanceNotFoundException.java
@@ -0,0 +1,7 @@
+package com.stgsporting.piehmecup.exceptions;
+
+public class AttendanceNotFoundException extends RuntimeException {
+    public AttendanceNotFoundException(String message) {
+        super(message);
+    }
+}
---
+++ src/main/java/com/stgsporting/piehmecup/exceptions/LiturgyNotFound.java
@@ -0,0 +1,7 @@
+package com.stgsporting.piehmecup.exceptions;
+
+public class LiturgyNotFound extends RuntimeException {
+    public LiturgyNotFound(String message) {
+        super(message);
+    }
+}
