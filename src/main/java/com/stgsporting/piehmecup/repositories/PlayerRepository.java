90ab7ce4289f50c89926c6522fbf09ebdb9e5884
Armia Joseph <armia.joseph35@gmail.com>
2025-02-17 23:58:29 +0200

    feat: sort the players desc

--- src/main/java/com/stgsporting/piehmecup/repositories/PlayerRepository.java
+++ src/main/java/com/stgsporting/piehmecup/repositories/PlayerRepository.java
@@ -3,11 +3,13 @@ package com.stgsporting.piehmecup.repositories;
 import com.stgsporting.piehmecup.entities.Player;
 import com.stgsporting.piehmecup.enums.Positions;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 import java.util.Optional;
 
 public interface PlayerRepository extends JpaRepository<Player,Long> {
     Optional<Player> findPlayerByName(String name);
+    @Query("SELECT p FROM PLAYERS p WHERE p.position = ?1 ORDER BY p.rating DESC")
     List<Player> findPlayersByPosition(Positions position);
 }
