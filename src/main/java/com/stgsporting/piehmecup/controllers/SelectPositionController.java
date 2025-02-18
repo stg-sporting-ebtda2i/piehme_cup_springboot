2fd5f7a7e25b0a2b0e7dcc8b9c621db4c585e520
Fadi Sarwat <fadisarwat@yahoo.com>
2025-02-19 01:32:22 +0200

    Remove redundant try/catch

--- src/main/java/com/stgsporting/piehmecup/controllers/SelectPositionController.java
+++ src/main/java/com/stgsporting/piehmecup/controllers/SelectPositionController.java
@@ -5,6 +5,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.Map;
+
 @RestController
 @RequestMapping("/selectPosition")
 public class SelectPositionController {
@@ -13,20 +15,12 @@ public class SelectPositionController {
 
     @GetMapping
     public ResponseEntity<Object> getSelectedPosition(){
-        try {
-            return ResponseEntity.ok().body(selectPositionService.getSelectedPosition());
-        } catch (Exception e){
-            return ResponseEntity.badRequest().body(e.getMessage());
-        }
+        return ResponseEntity.ok().body(selectPositionService.getSelectedPosition());
     }
 
     @PatchMapping("/{positionId}")
     public ResponseEntity<Object> selectPosition(@PathVariable Long positionId){
-        try {
-            selectPositionService.selectPosition(positionId);
-            return ResponseEntity.ok().body("Position selected successfully");
-        } catch (Exception e){
-            return ResponseEntity.badRequest().body(e.getMessage());
-        }
+        selectPositionService.selectPosition(positionId);
+        return ResponseEntity.ok().body(Map.of("message", "Position selected"));
     }
 }
