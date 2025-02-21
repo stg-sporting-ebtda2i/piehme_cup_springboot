e29db825dff5285253407f9657abf8702263e4f0
Fadi Sarwat <fadisarwat@yahoo.com>
2025-02-22 00:24:14 +0200

    WIP

--- src/test/java/com/stgsporting/piehmecup/PiehmecupApplicationTests.java
+++ src/test/java/com/stgsporting/piehmecup/PiehmecupApplicationTests.java
@@ -2,7 +2,9 @@ package com.stgsporting.piehmecup;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.ComponentScan;
 
+@ComponentScan(basePackages = "com.stgsporting.piehmecup")
 @SpringBootTest
 class PiehmecupApplicationTests {
 
