f35dfebed9d2c60afcd9baa7ed4184148c852e14
Fadi Sarwat <fadisarwat@yahoo.com>
2025-02-24 15:38:15 +0200

    Increase size of file upload

---
+++ src/main/java/com/stgsporting/piehmecup/config/MultipartConfig.java
@@ -0,0 +1,21 @@
+package com.stgsporting.piehmecup.config;
+import jakarta.servlet.MultipartConfigElement;
+import org.springframework.boot.web.servlet.MultipartConfigFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.util.unit.DataSize;
+
+@Configuration
+public class MultipartConfig {
+
+    @Bean
+    public MultipartConfigElement multipartConfigElement() {
+        MultipartConfigFactory factory = new MultipartConfigFactory();
+
+        factory.setMaxFileSize(DataSize.ofMegabytes(100));
+
+        factory.setMaxRequestSize(DataSize.ofMegabytes(100));
+
+        return factory.createMultipartConfig();
+    }
+}
