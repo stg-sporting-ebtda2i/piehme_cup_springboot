ee8034b78e1a68df61e85f4615f356ce16098c06
Armia Joseph <armia.joseph35@gmail.com>
2025-03-03 00:29:45 +0200

    feat: cant attend more than once in a week

---
+++ src/main/java/com/stgsporting/piehmecup/exceptions/InvalidAttendanceException.java
@@ -0,0 +1,7 @@
+package com.stgsporting.piehmecup.exceptions;
+
+public class InvalidAttendanceException extends RuntimeException {
+    public InvalidAttendanceException(String message) {
+        super(message);
+    }
+}
